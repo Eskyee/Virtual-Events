9af48fbf78ddc7d73cdc8da86a5e872c11eaeb87
Esky33 <djescaba@icloud.com>
2022-08-09 15:44:11 +0100

    update meta info

--- lib/constants.ts
+++ lib/constants.ts
@@ -18,13 +18,13 @@ export const SITE_URL = 'http://virtual-events-9j50r4s0q-jungle-lab.vercel.app/a
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
 export const TWITTER_USER_NAME = 'junglelab_io';
 export const BRAND_NAME = 'JUNGLELABᵀᴹ';
-export const SITE_NAME_MULTILINE = ['JUNGLELABᵀᴹ', 'Live Events'];
-export const SITE_NAME = 'JUNGLELABᵀᴹ Live Events';
+export const SITE_NAME_MULTILINE = ['JUNGLELABᵀᴹ', 'Virtual Live Events'];
+export const SITE_NAME = 'JUNGLELABᵀᴹ Virtual Live Events';
 export const META_DESCRIPTION =
   'JungleLab is an artist-driven Digital Assets Marketplace. Making it easy for digital creators to authenticate, showcase and sell the digital assets they produce on the Liquid Blockchain.';
 export const SITE_DESCRIPTION =
   'An interactive online experience by the community, free for everyone.';
-export const DATE = 'PLAN ₿ FORUM 28-29 OCTOBER 2022, LUGANO 28-29';
+export const DATE = 'PLAN ₿ FORUM 28-29 OCTOBER 2022, LUGANO';
 export const SHORT_DATE = 'OCT 28-29 - 9:00am PST';
 export const FULL_DATE = 'OCT 28-29 9am Pacific Time (GMT-7)';
 export const TWEET_TEXT = META_DESCRIPTION;
