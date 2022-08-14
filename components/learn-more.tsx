6ebae51a5e88a2a49433552e11c85c16bde7b3f2
Esky33 <djescaba@icloud.com>
2022-08-14 06:44:49 +0100

    add info

--- components/learn-more.tsx
+++ components/learn-more.tsx
@@ -22,7 +22,7 @@ import { REPO } from '@lib/constants';
 export default function LearnMore() {
   return (
     <div className={cn(styleUtils.appear, styleUtils['appear-fifth'], styles.contact)}>
-      Learn more on{' '}
+      Learn more on our{' '}
       <a href={REPO} className={styles['contact-email']} target="_blank" rel="noopener noreferrer">
         Blog
       </a>
